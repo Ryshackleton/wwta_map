b59b1e2df5c896d2fb08c2ae35e4c11ea1725532
Ryan Shackleton <ryshack@uw.edu>
2018-10-21 15:28:26 -0700

    Add functionality to cluster markers by layer.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,51 @@
 
+/** mappings for layer/icon types */
+var colorByTyp = {
+  site: 'rgba(181, 226, 140, 0.6)',
+  access: 'rgba(240, 194, 12, 0.6)',
+};
+
+// lookup of type (site, access) -> icon to represent the type
+var iconsByTypProp = {
+  site: new L.Icon({
+    iconSize: [23, 23],
+    iconAnchor: [13, 23],
+    popupAnchor: [1, -24],
+    iconUrl: 'resources/campground-solid.svg'
+  }),
+  access: new L.Icon({
+    iconSize: [23, 23],
+    iconAnchor: [13, 23],
+    popupAnchor: [1, -24],
+    iconUrl: 'resources/ship-solid.svg',
+  }),
+};
+
+var faIconHtmlByTypProp = {
+  site: '<i class="fas fa-campground"></i>',
+  access: '<i class="fas fa-ship"></i>',
+};
+
+var typLabel = {
+  site: 'WWTA Campsite',
+  access: 'Public Boat Ramps',
+}
+
+// maps what to do for each feature in the Leaflet layer
+var layerGeoJsonOptions = {
+  // which icons go with each layer
+  pointToLayer: function(feature, latlng) {
+    return L.marker(latlng, {
+      icon: iconsByTypProp[feature.properties.typ]
+    });
+  },
+  // for each feature, bind a popup to the marker in the map that
+  // contains the tooltipContent defined above
+  onEachFeature: function (feature, layer) {
+    layer.bindPopup(feature.properties.tooltipContent);
+  }
+};
+
 // converts an array of markers to an array of GeoJson Point features
 // also creates the tooltip content/ updates the URLs to point to the wwta website
 // not the server we're currently on
@@ -21,42 +68,58 @@ function markersToGeojsonArray(markerArray) {
     };
   });
 }
+function iconClusterOptions(clusterTyp) {
+  return {
+    maxClusterRadius: 120,
+    iconCreateFunction: function (cluster) {
+      var markers = cluster.getAllChildMarkers();
+      var childCount = markers.length;
+      var typ = cluster.getAllChildMarkers()[0].feature.properties.typ;
+      var clusterClass = 'leaflet-marker-icon marker-cluster leaflet-zoom-animated leaflet-interactive'
+      var icon = faIconHtmlByTypProp[typ];
+      return L.divIcon({
+        html: '<div><span>' + childCount + ' ' + icon + '</span></div>',
+        className: clusterClass + ' marker-cluster-' + typ,
+        iconSize: L.point(40, 40),
+      });
+    },
+    polygonOptions: {
+      className: 'blurredPolygon',
+      stroke: false,
+      fillColor: colorByTyp[clusterTyp],
+      fillOpacity: 1,
+    },
+  };
+}
 
-// lookup of type (site, access) -> icon to represent the type
-var iconsByTypProp = {
-  site: new L.Icon({
-    iconSize: [27, 27],
-    iconAnchor: [13, 27],
-    popupAnchor: [1, -24],
-    iconUrl: 'resources/icons8-camping-tent-50.png'
-  }),
-  access: new L.Icon({
-    iconSize: [27, 27],
-    iconAnchor: [13, 27],
-    popupAnchor: [1, -24],
-    iconUrl: 'resources/water-access-icon.png'
-  }),
-};
+/** function to add an svg blur to the polygons that show up when the clusters are hovered over */
+function addSVGBlurToPolygons(basemap) {
+  // must add a polygon to the map for the overlay layer to be created,
+  // so add a random one to the north pole somewhere out of view
+  var someLineIntheNorthPole = [[-126.562500,84.802474],[-122.343750,84.802474]];
+  var polygon = L.polygon(someLineIntheNorthPole).addTo(basemap.leafletmap());
 
-// maps what to do for each feature in the Leaflet layer
-var layerGeoJsonOptions = {
-  // which icons go with each layer
-  pointToLayer: function(feature, latlng) {
-    return L.marker(latlng, {
-      icon: iconsByTypProp[feature.properties.typ]
-    });
-  },
-  // for each feature, bind a popup to the marker in the map that
-  // contains the tooltipContent defined above
-  onEachFeature: function (feature, layer) {
-    layer.bindPopup(feature.properties.tooltipContent);
-  }
-};
+  var svg = basemap.leafletmap().getPanes().overlayPane.firstChild;
+  var svgFilter = document.createElementNS('http://www.w3.org/2000/svg', 'filter');
+  var svgBlur = document.createElementNS('http://www.w3.org/2000/svg', 'feGaussianBlur');
+
+  svgFilter.setAttribute('id', 'blur');
+  svgFilter.setAttribute('x', '-100%');
+  svgFilter.setAttribute('y', '-100%');
+  svgFilter.setAttribute('width', '500%');
+  svgFilter.setAttribute('height', '500%');
+  svgBlur.setAttribute('stdDeviation', 5);
+
+  svgFilter.appendChild(svgBlur);
+  svg.appendChild(svgFilter);
+}
 
 window.onload = function() {
-  var basemap = new LeafletBasemap({ mapDivId: 'mapContainer' })
+  var basemap = window.basemap = new LeafletBasemap({ mapDivId: 'mapContainer', layersControlOptions: { collapsed: false } })
     .addESRIWorldImageryBaseMapLayer()
     .addESRIOceanBaseMapLayer();
+
+  addSVGBlurToPolygons(basemap);
 
   // navigational charts, will be transparent and always as an optional overlay
   var navCharts = L.esri.imageMapLayer({
@@ -95,19 +158,28 @@ window.onload = function() {
       Object.keys(markersByLayer).forEach(function (layerKey) {
         // for each layer, convert all marker objects to GeoJson format
         var layerGeoJson = markersToGeojsonArray(markersByLayer[layerKey]);
+
         // create a leaflet layer from the GeoJson for each layer
         var thisLayer = L.geoJson(
           layerGeoJson,
           layerGeoJsonOptions
-        ).addTo(basemap.leafletmap());
+          );
+
+        // build the marker clusterer
+        var clusterOptions = Object.assign({}, iconClusterOptions(layerKey), { typ: layerKey });
+        var cluster = L.markerClusterGroup(clusterOptions);
+        cluster.addLayer(thisLayer);
+
+        basemap.leafletmap().addLayer(cluster);
 
         // update the bounds so we can zoom to fit the layers
         var layerBounds = thisLayer.getBounds();
         bounds.extend(layerBounds);
 
         // add this layer to the layer control (checkboxes to toggle layers)
-        basemap.layerControl().addOverlay(thisLayer, layerKey);
+        basemap.layerControl().addOverlay(cluster, typLabel[layerKey] + ' ' + faIconHtmlByTypProp[layerKey]);
       });
+
       // zoom to the bounds of all layers
       basemap.leafletmap().fitBounds(bounds);
     }
